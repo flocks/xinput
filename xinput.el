4a85c49fd8cce6ef7e3fd6c68a2dbe2fed8267e7
Florent Teissier <teissierflorent@gmail.com>
2022-08-19 17:59:24 +0200

    some cleaing + init readme

--- xinput.el
+++ xinput.el
@@ -1,12 +1,48 @@
-(defcustom xinput--regex-line-device "^[^[:alnum:]]*\\(.*\\).*id=\\([[:digit:]]*\\)"
-  "Regex used to match device's name and id"
+
+
+;; Author: Florent Teissier <teissierflorent@gmail.com>
+;; Keywords: xinput, keyboard, mouse
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is a small package interfacing xinput command
+;; It displays the output of xinput in a table and allows
+;; you to set props to a specific device
+
+;;; Code:
+
+(defcustom xinput--regex-line-device "^[^[:alnum:]]*\\(.*\\).*id=\\([[:digit:]]*\\)\t\\[\\(.*\\).*\]"
+  "Regex used to match device's name and id and master/slave "
   :type 'regexp)
 
 (defcustom xinput--regex-line-props  "^[^[:alnum:]]*\\(.*\\):[^[:alnum:]]\\(.*\\)"
   "Regex used to match props name and value"
   :type 'regexp)
 
+(defcustom xinput--props-buffer-name "*xinput-props*"
+  "Name of main xinput props buffer "
+  :type 'string)
+
+(defcustom xinput--main-buffer-name "*xinput*"
+  "Name of main xinput buffer "
+  :type 'string)
+
 (defun xinput--get-devices-list ()
+  "Run xinput and parse the output to get a list of elements of form
+'(NAME ID)"
   (let ((result))
 	(with-temp-buffer
 	  (insert (shell-command-to-string "xinput"))
@@ -14,16 +50,22 @@
 	  (while (not (eobp))
 		(let* ((line (thing-at-point 'line)) 
 			   (match (string-match xinput--regex-line-device line)))
-		  (push `(,(string-trim (match-string 1 line)) ,(match-string 2 line)) result))
+		  (push `(,(string-trim (match-string 1 line))
+				  ,(match-string 2 line)
+				  ,(match-string 3 line))
+				result))
 		(forward-line)))
 	result))
 
 
 (defun xinput--format-devices-list (devices)
+  "Format the list of DEVICES of form '((NAME ID) (NAME ID)) for
+tabulated-list-mode."
   (let ((result))
 	(dolist (device devices)
 	  (push `(,(cadr device) ,(vector  (xinput--propertize-id (cadr device))
-									   (xinput--propertize-name (car device))))
+									   (xinput--propertize-name (car device))
+									   (propertize (caddr device) 'font-lock-face 'font-lock-doc-face)))
 			result))
 	result))
 
@@ -31,25 +73,13 @@
   (propertize id 'face 'bold))
 
 (defun xinput--propertize-name (name)
-  (propertize name 'font-lock-face 'font-lock-comment-face))
+  (propertize name 'font-lock-face 'font-lock-doc-face))
 
-(defun xinput-view-device (device)
+(defun xinput-view-device-props (device)
   (interactive
    (list (or (tabulated-list-get-id)
 			 (read-string "Device ID: "))))
-  (with-current-buffer (get-buffer-create (format "*xinput-device*" device))
-	(let ((inhibit-read-only t))
-	  (erase-buffer)
-	  (insert (shell-command-to-string (format "xinput list-props %s" device)))
-	  (goto-char (point-min)))
-	(special-mode)
-	(switch-to-buffer-other-window (current-buffer))))
-
-(defun xinput-view-device (device)
-  (interactive
-   (list (or (tabulated-list-get-id)
-			 (read-string "Device ID: "))))
-  (with-current-buffer (get-buffer-create (format "*xinput-device* %s" device))
+  (with-current-buffer (get-buffer-create (format "%s %s" xinput--props-buffer-name device))
 	(setq xinput--global-device-id device)
 	(xinput-props-mode)
 	(switch-to-buffer (current-buffer))))
@@ -60,7 +90,8 @@
 	(shell-command (read-shell-command "Run: "
 						 (format "xinput set-prop %s \"%s\" "
 								 xinput--global-device-id
-								 prop-name)))))
+								 prop-name)))
+	(xinput-props-mode)))
 
 (define-derived-mode xinput-mode tabulated-list-mode "XInput"
   "Special mode managing xinput"
@@ -68,7 +99,8 @@
   (setq major-mode 'xinput-mode)
   (use-local-map xinput-mode-map)
   (setq tabulated-list-format [("id" 10 t)
-							   ("Name" 60 t)])
+							   ("Name" 60 t)
+							   ("Master/Slave" 60 t)])
 
   (setq tabulated-list-entries (xinput--format-devices-list (xinput--get-devices-list)))
   (setq tabulated-list-padding 2)
@@ -119,10 +151,27 @@
   (hl-line-mode 1)
   (run-mode-hooks 'xinput-props-mode-hook))
 
-(let ((map xinput-mode-map))
-  (evil-define-key 'motion map
-	(kbd "RET") #'xinput-view-device))
+(defun xinput ()
+  "Point of entry, create a buffer and trigger xinput-mode"
+  (interactive)
+  (let ((buff (get-buffer-create xinput--main-buffer-name)))
+	(with-current-buffer buff
+	  (xinput-mode))
+	(pop-to-buffer-same-window buff)))
 
-(let ((map xinput-props-mode-map))
-  (evil-define-key 'motion map
-	(kbd "RET") #'xinput-set-prop))
+(if (boundp 'evil-mode)
+	(progn
+	  (let ((map xinput-mode-map))
+		(evil-define-key 'motion map (kbd "RET") #'xinput-view-device-props))
+	  (let ((map xinput-props-mode-map))
+		(evil-define-key 'motion map (kbd "RET") #'xinput-set-prop))
+	  )
+  (progn
+	(let ((map xinput-mode-map))
+	  (define-key map (kbd "RET") #'xinput-view-device-props))
+
+	(let ((map xinput-props-mode-map))
+	  (define-key map (kbd "RET") #'xinput-set-prop))
+	))
+
+(provide 'xinput)
